197a4d1cb7af460fbf657c7d68d42feb9bcf851c
wssyy1995 <18221091524@126.com>
2019-08-28 11:29:23 +0800

    third push

--- run_test.py
+++ run_test.py
@@ -9,6 +9,6 @@ discover=unittest.defaultTestLoader.discover(test_dir,pattern='baidu*.py')
 nowtime=time.strftime("%Y-%m-%d %H:%M:%S")
 filename='./'+nowtime+'.html'
 fp=open(filename,'wb')
-runner=HTMLTestRunner(stream=fp,title='百度搜索',description='the report of baidu-search')
+runner=HTMLTestRunner(stream=fp,title='baidutest',description='the report of baidu-search')
 runner.run(discover)
 fp.close()
