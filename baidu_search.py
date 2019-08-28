c7dc2a75dffefa17ca493b85d3e1b0b1a92397a2
wssyy1995 <18221091524@126.com>
2019-08-28 10:33:02 +0800

    second push

--- baidu_search.py
+++ baidu_search.py
@@ -12,15 +12,23 @@ class Baidu(unittest.TestCase):
         self.driver.get("http://www.baidu.com")
 
 
-    def test_baidu_search(self):
+    def test_baidu_search1(self):
 
-        self.driver.find_element_by_id('kw').send_keys('test1')
+        self.driver.find_element_by_id('kw').send_keys('jenkins_test1')
         time.sleep(1)
         self.driver.find_element_by_id('su').click()
         time.sleep(1)
         print(self.driver.title)
-        self.assertEqual(self.driver.title,'test1_百度搜索')
+        self.assertEqual(self.driver.title,'jenkins_test1_百度搜索')
 
+    def test_baidu_search2(self):
+
+        self.driver.find_element_by_id('kw').send_keys('jenkins_test2')
+        time.sleep(1)
+        self.driver.find_element_by_id('su').click()
+        time.sleep(1)
+        print(self.driver.title)
+        self.assertEqual(self.driver.title,'jenkins_test2_百度搜索')
 
     def tearDown(self):
         self.driver.quit()
